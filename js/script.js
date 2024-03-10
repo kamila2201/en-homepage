37a2479e506040ac3fe0bd5653a6ad580699139b
Kamila Maciąg <kamila.maciag22@gmail.com>
2024-03-10 14:42:12 +0100

    added all functions to the block

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-const welcome = () => {
-    console.log("Hello everyone!");
-}
-
-const toggleBackground = () => {
-    const body = document.querySelector(".js-body");
-    const themeName = document.querySelector(".js-themeName");
-
-    body.classList.toggle("body--color");
-
-    themeName.innerText = body.classList.contains("body--color")
-        ? "image"
-        : "color";
-};
-
-const init = () => {
-    const button = document.querySelector(".js-button");
-    button.addEventListener("click", toggleBackground);
+{
+    const welcome = () => {
+        console.log("Hello everyone!");
+    }
     
-    welcome(); 
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName");
+    
+        body.classList.toggle("body--color");
+    
+        themeName.innerText = body.classList.contains("body--color")
+            ? "image"
+            : "color";
+    };
+    
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
+        
+        welcome(); 
+    }
+    
+    init();
 }
-
-init();
